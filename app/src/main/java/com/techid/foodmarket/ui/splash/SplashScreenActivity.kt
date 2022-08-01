83dfac96a2ccbf22678c802d2ddf8cb4759182ed
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-01 20:59:58 +0700

    success make splash screen

---
+++ app/src/main/java/com/techid/foodmarket/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.techid.foodmarket.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.techid.foodmarket.MainActivity
+import com.techid.foodmarket.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+                              startActivity(Intent(this, MainActivity::class.java))
+        }, 3000)
+    }
+
+}
